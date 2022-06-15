d105f97c09297fb621fd3dfc6b7718a741fb42dd
Diego Goez <dafernandez56@misena.edu.co>
2022-06-14 23:41:01 -0500

    modificacion en erase LISTO

--- view/module/erase.php
+++ view/module/erase.php
@@ -26,4 +26,30 @@ function eraseUser(){
 
 }
 
+eraseAprendiz();
+function eraseAprendiz(){
+    try {
+        $objDtoAprendiz = new Aprendiz();
+        $objDtoAprendiz -> setCode($_GET['codigo']);
+        $objDaoAprendiz = new AprendizModel($objDtoAprendiz);
+        if ( $objDaoAprendiz -> mldEraseAprendiz() == true ) {
+
+            echo "<script>
+                    Swal.fire(
+                        'Borrado!',
+                        'El registro ha sido borrado',
+                        'success'
+                    )
+                </script>";
+                include_once 'view/module/user.php'; 
+        }
+
+    } catch ( PDOException $e ) {
+        echo "Error on the delete of the 
+        controller of show all " . $e->getMessage(); 
+    }
+
+
+}
+
 ?>
