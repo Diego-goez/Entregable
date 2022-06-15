2496ae83bff4931ef40ec43c353adf27a1510d04
Diego Goez <dafernandez56@misena.edu.co>
2022-06-14 23:07:53 -0500

    modificacion validate con aprendiz LISTO

--- view/js/validate.js
+++ view/js/validate.js
@@ -70,3 +70,77 @@ function validateModify(e){
     
 
 }
+
+function validateAprendiz(e){
+    e.preventDefault();
+    formulario    = document.getElementById('frmAprendiz');
+    nombre        = document.getElementById('txtNombre');
+    fechanaci     = document.getElementById('txtFechanaci');
+    sexo          = document.getElementById('txtSexo');
+    ciudad        = document.getElementById('txtCiudad');
+    
+    lVali = true;
+    
+    if (nombre.value==""){
+        nombre.style.borderColor="red";
+        ohSnap('Ingresar el nombre...', {color: 'red'});  // alert will have class 'alert-color'
+        lVali = false;
+    }
+    if (fechanaci.value==""){
+        fechanaci.style.borderColor="red";
+        ohSnap('Ingresar la fecha de nacimiento...', {color: 'red'},{duration:1000});  // alert will have class 'alert-color'
+        lVali = false;
+    }
+    if (sexo.value==""){
+        sexo.style.borderColor="red";
+        ohSnap('Ingresar el sexo...', {color: 'red'});  // alert will have class 'alert-color'
+        lVali = false;
+    }
+    if (ciudad.value==""){
+        ciudad.style.borderColor="red";
+        ohSnap('Ingresar la ciudad...', {color: 'red'});  // alert will have class 'alert-color'
+        lVali = false;
+    }
+    if (lVali == true){
+        formulario.submit();
+    }
+    
+
+}
+function validateModifyAprendiz(e){
+    e.preventDefault();
+    formulario  = document.getElementById('frmAprendizModificar');
+    nombre      = document.getElementById('txtNombreM');
+    fechanaci   = document.getElementById('txtFechanaciM');
+    sexo        = document.getElementById('txtSexoM');
+    ciudad      = document.getElementById('txtCiudadM');
+    
+    lVali = true;
+    
+    if (nombre.value==""){
+        nombre.style.borderColor="red";
+        ohSnap('Ingresar el nombre...', {color: 'red'});  // alert will have class 'alert-color'
+        lVali = false;
+    }
+    if (fechanaci.value==""){
+        fechanaci.style.borderColor="red";
+        ohSnap('Ingresar la fecha de nacimiento...', {color: 'red'},{duration:1000});  // alert will have class 'alert-color'
+        lVali = false;
+    }
+    if (sexo.value==""){
+        sexo.style.borderColor="red";
+        ohSnap('Ingresar el sexo...', {color: 'red'});  // alert will have class 'alert-color'
+        lVali = false;
+    }
+    if (ciudad.value==""){
+        ciudad.style.borderColor="red";
+        ohSnap('Ingresar la ciudad...', {color: 'red'});  // alert will have class 'alert-color'
+        lVali = false;
+    }
+    if (lVali == true){
+        formulario.submit();
+    }
+    
+
+}
+
